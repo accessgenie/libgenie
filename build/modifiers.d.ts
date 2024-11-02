1db4f553caf4974af4d3943a9bf2886dc02404da
Marek Małek <12412949+mmalek06@users.noreply.github.com>
2024-11-02 17:52:31 +0100

    Task/add binary to modifier (#25)

--- build/modifiers.d.ts
+++ build/modifiers.d.ts
@@ -1,2 +1,2 @@
 export declare function mapBinaryValue(input: string, values: string[]): string;
-export declare function capitalize(input: string): string;
+export declare function capitalize(input?: string): string | undefined;
