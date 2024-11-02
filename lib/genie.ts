e0cbcdb8af8a049baecec69eec200ad7cb44fe71
Marek Małek <12412949+mmalek06@users.noreply.github.com>
2024-11-02 17:42:34 +0100

    Task/add binary to modifier (#23)

--- lib/genie.ts
+++ lib/genie.ts
@@ -55,7 +55,7 @@ export function applyModifier(data: any, modifier: Modifier[]): any {
         break;
       case 'ascii':
         result = deburr(result).replace(/[^\x00-\x7F]/g, '');
-      case 'binaryTo':
+      case 'mapBinaryValue':
         result = mapBinaryValue(result, item.arguments.values);
     }
   }
